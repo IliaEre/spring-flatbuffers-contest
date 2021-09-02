b8a8a5f2516c0e37e93ca29af6e0fd31e7aa57a6
Ilia Ivankin <ilia_ivankin@epam.com>
2021-09-02 13:07:30 +0300

    init services

---
+++ src/main/kotlin/com/epam/contest/flatbufferdemo/FlatbufferDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.epam.contest.flatbufferdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlatbufferDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlatbufferDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/epam/contest/flatbufferdemo/FlatbufferDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.epam.contest.flatbufferdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlatbufferDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
